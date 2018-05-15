b9c788abded3bcaeafd34fae3288d510e493a880
Ram kandimalla <ram@Rams-MacBook-Air.local>
2018-05-16 03:54:05 +0530

    updated code

--- mnist_numpy_cnn.py
+++ mnist_numpy_cnn.py
@@ -42,19 +42,24 @@ print ("test_images shape = ", test_images.shape)
 print ('test_labels shape = ', test_labels.shape)
 
 
-def pad(x, pad):
+#padding with Zeros
 
-     train_images= np.pad(x,((0,0),(pad,pad),(pad,pad),(0,0)),'constant',constant_values=(0,0))
+def padding(x, pad):
+
+     x_pad= np.pad(x,((0,0),(pad,pad),(pad,pad),(0,0)),'constant',constant_values=(0,0))
      
-     return train_images.shape
+     return x_pad
  
-#pad(train_images,2)
-
+    
+    
+#Convolution
 def convolution(a, w, b):
     
     s = np.multiply(a, w) + b
 
     z = np.sum(s)
+    
+    z = z+float(b)
 
     return z
 
@@ -62,67 +67,64 @@ def convolution(a, w, b):
 def conv_forward(A_prev, W, b, hparameters):
     
     
-    """
-    Implements the forward propagation for a convolution function
-    
-    Arguments:
-    A_prev -- output activations of the previous layer, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
-    W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
-    b -- Biases, numpy array of shape (1, 1, 1, n_C)
-    hparameters -- python dictionary containing "stride" and "pad"
-        
-    Returns:
-    Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
-    cache -- cache of values needed for the conv_backward() function
-    """
-    
-    
-    ### START CODE HERE ###
-    # Retrieve dimensions from A_prev's shape (≈1 line)  
+  
+    # Retrieve dimensions from A_prev shape  
     (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
     
-    # Retrieve dimensions from W's shape (≈1 line)
+    # Retrieve dimensions from W shape 
     (f, f, n_C_prev, n_C) = W.shape
 
-    # Retrieve information from "hparameters" (≈2 lines)
-    stride = hparameters['stride']
-    pad = hparameters['pad']
+    # Retrieve information from hyper parameters" 
+    stride = hyper_parameters['stride']
+    pad = hyper_parameters['pad']
     
-    # Compute the dimensions of the CONV output volume using the formula given above. Hint: use int() to floor. (≈2 lines)
+    # Convolve dimentions of next layer
     n_H = int((n_H_prev - f + 2 * pad) / stride) + 1
     n_W = int((n_W_prev - f + 2 * pad) / stride) + 1
     
-    # Initialize the output volume Z with zeros. (≈1 line)
+    # Initialize the output volume Z with zeros
     Z = np.zeros((m, n_H, n_W, n_C))
     
     # Create A_prev_pad by padding A_prev
-    A_prev_pad = pad(A_prev, pad)
+    A_prev_pad = padding(A_prev, pad)
     
     for i in range(m):                                 # loop over the batch of training examples
         a_prev_pad = A_prev_pad[i]                     # Select ith training example's padded activation
         for h in range(n_H):                           # loop over vertical axis of the output volume
             for w in range(n_W):                       # loop over horizontal axis of the output volume
                 for c in range(n_C):                   # loop over channels (= #filters) of the output volume
-                    # Find the corners of the current "slice" (≈4 lines)
+                    # Find the corners of the current frame 
                     vert_start = h * stride
                     vert_end = vert_start + f
                     horiz_start = w * stride
                     horiz_end = horiz_start + f
-                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                    # Use the corners to define the frame of a_prev_pad
                     a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
-                    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈1 line)
+                    # Convolve the frame with the correct filter W and bias b, to get back one output neuron.
                     Z[i, h, w, c] = convolution(a_slice_prev, W[...,c], b[...,c])
                                         
-    ### END CODE HERE ###
+
 
     # Making sure your output shape is correct
     assert(Z.shape == (m, n_H, n_W, n_C))
     
-    # Save information in "cache" for the backprop
+    # Save information in cache for the backpropagation
     cache = (A_prev, W, b, hparameters)
     
     return Z, cache
 
+
+np.random.seed(1)
+A_prev = np.random.randn(10, 4, 4, 3)
+W = np.random.randn(2, 2, 3, 8)
+b = np.random.randn(1, 1, 1, 8)
+hyper_parameters = {"pad" : 2,
+               "stride": 1}
+
+Z, cache_conv = conv_forward(A_prev, W, b, hyper_parameters)
+
+#print Z
+#print cache_conv
 
 
 
